d8452488258b3a3d183fb5e61db76bea97b0d78a
LucasZ <godadzeluca@gmail.com>
2026-06-18 17:43:52 +0400

    Delete 11 directory

--- 11/init_db.py
+++
@@ -1,5 +0,0 @@
-from ext import app, db
-from models import Recipe, Review
-
-with app.app_context():
-    db.create_all()
--- 11/run.py
+++
@@ -1,5 +0,0 @@
-from ext import app
-
-from routes import *
-
-app.run(host="0.0.0.0")
